8da17862d0ae7f42501a2ccc1d14105e04e19efe
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-24 23:18:21 +0600

    update

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected successfully')
+        const database = client.db("doctors_portal");
+        const appointmentsCollection = database.collection("appointments");
+
+        app.post('/appointments', async (req, res) => {
+
+        })
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
