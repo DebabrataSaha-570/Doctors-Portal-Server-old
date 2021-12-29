0fcd74f5c0d51698b871d3728eff2d70dc692a5c
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2021-12-29 23:20:34 +0600

    update

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ async function run() {
             const appointments = await cursor.toArray()
             res.json(appointments)
         })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true;
+
+            }
+            res.json({ admin: isAdmin })
+        })
 
         app.post('/appointments', async (req, res) => {
             const appointment = req.body;
@@ -56,6 +67,16 @@ async function run() {
             const updateDoc = { $set: user };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result)
+        })
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user)
+            const filter = { email: user.email }
+            const updateDoc = { $set: { role: 'admin' } }
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+
+
         })
     } finally {
         //   await client.close();
